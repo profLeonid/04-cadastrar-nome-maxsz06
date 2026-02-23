ca44d7e0e347f04c266f4767b11e0b9987b6a57d
MAXWILLIAN ALBINO ZACARIAS DE SANTANA <25203648@CFP127ALUNO.LOCAL>
2026-02-23 10:11:38 -0300

    Aula concluida

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-'use scrict'
+'use strict'
+
+const cadastrar = document.getElementById('cadastrar') // Pegar o Elemento pelo id()
+
+function cadastraNome(){
+
+    const nome= document.getElementById('nome')
+    const listaNomes = document.getElementById('lista-nomes')
+
+    const itemNome = document.createElement('p')
+    itemNome.textContent = nome.value
+
+    listaNomes.appendChild(itemNome)
+}
+
+cadastrar.addEventListener('click',cadastraNome)
